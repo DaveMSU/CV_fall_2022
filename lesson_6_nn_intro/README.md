e99dd62de1839a5459381080eac00e171954b358
DaveMSU <davehum@mail.ru>
2022-10-28 20:26:03 +0600

    Working solution for 9.5 score on the test.

---
+++ lesson_6_nn_intro/README.md
@@ -0,0 +1,3 @@
+For creating detection.py file use command:
+    python create_detection.py --net=./configs/architectures/simple_cnn.json --learn=./configs/learning_process/base.json
+
--- lesson_6_nn_intro/dataset.py
+++ lesson_6_nn_intro/dataset.py
@@ -10,12 +10,12 @@ from matplotlib import pyplot as plt
 
 class ImagePointsDataset(torch.utils.data.Dataset):
     def __init__(
-        self,
-        mode: str,
-        train_fraction: float,
-        data_dir: pathlib.PosixPath,
-        train_gt: tp.Dict[str, np.ndarray],
-        new_size: tp.Tuple[int, int] = (64, 64)
+            self,
+            mode: str,
+            train_fraction: float,
+            data_dir: pathlib.PosixPath,
+            train_gt: tp.Dict[str, np.ndarray],
+            new_size: tp.Tuple[int, int] = (64, 64)
     ):
         assert data_dir.exists(), f"Dir '{data_dir}' do not exists!"
         
@@ -26,25 +26,36 @@ class ImagePointsDataset(torch.utils.data.Dataset):
             }
             for file_name, points in train_gt.items()
         ]
+
+        np.random.seed(7)
+        self._items = np.random.permutation(self._items)
         
         for pair in self._items:
             assert pair["path"].exists(), f"File '{pair['path']}' do not exists!"
             
         train_size = round(len(self._items) * train_fraction)
-        
+
+        self._mode = mode        
         if mode == "train":
             self._items = self._items[:train_size]
         elif mode == "val":
             self._items = self._items[train_size:]
+        elif mode == "test":
+            pass
         else:
             assert f"Mode '{mode}' undefined!"
             
         self._X_size = new_size[0]
         self._Y_size = new_size[1]
+
+    
+    @property
+    def paths(self) -> tp.List[str]:
+        return [pair["path"] for pair in self._items]
         
     
     @property
-    def shape(self):
+    def shape(self) -> tp.Tuple[int, int]:
         return (self._X_size, self._Y_size)
     
     
@@ -60,19 +71,22 @@ class ImagePointsDataset(torch.utils.data.Dataset):
         img_path = pair["path"]
         target = pair["target"].copy()
 
-        ## read image 
+        ## Load image.
         image = Image.open(img_path).convert('RGB')
         image = np.array(image).astype(np.float32) / 255.
         
-        # Change shapes
+        # Change shapes.
         orig_shape = image.shape
         self._items[index]["shape"] = orig_shape
         image = cv2.resize(image, (self._X_size, self._Y_size))
-        target[::2] = target[::2] / orig_shape[0]
-        target[1::2] = target[1::2] / orig_shape[1]
+        if self._mode != "test":
+            target[::2] = target[::2] / orig_shape[0]
+            target[1::2] = target[1::2] / orig_shape[1]
         
-        # to tensor
+        # Convert to tensor.
         image = torch.from_numpy(image.transpose(2, 0, 1))
+        if image.size(0) == 1:
+            image = image.repeat(3, 1, 1)        
         target = torch.from_numpy(target.astype(np.float32))
         
         return image, target, torch.Tensor(orig_shape)
