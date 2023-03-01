ad5eda91cdf9cdb7507b88f6dd669087286c9538
DaveMSU <davehum@mail.ru>
2023-03-01 17:41:05 +0300

    Forgot to add command.sh's

---
+++ lesson_6_nn_intro/experiments/sgd_7.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/david_tyuman/my_github/cv_fall_2022\
+    && source ~/.pyenv/versions/3.8.2/envs/mac_env/bin/activate\
+    && python make_pool.py --config=./lesson_6_nn_intro/configs/pool_making/base.json\
+    && python train.py --net=./lesson_6_nn_intro/configs/architectures/simple_cnn.json --learn=./lesson_6_nn_intro/configs/learning_process/sgd_7.json
