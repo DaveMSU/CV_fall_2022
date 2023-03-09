a7c791e98e63cec7051bf76cca0655d60a9ca029
DaveMSU <davehum@mail.ru>
2023-03-09 10:11:02 +0300

    Now there is no practice to keep exps in code, thus certain revision is self-sufficient.

--- lesson_6_nn_intro/experiments/adam_0001.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir /var/lib/storage/resources/cv_fall_2022/lesson_6_nn_intro/experiments/adam_0001 \
-    && cd /home/david_tyuman/my_github/cv_fall_2022 \
-    && source ~/.pyenv/versions/3.8.2/envs/mac_env/bin/activate \
-    && python make_pool.py --config=./lesson_6_nn_intro/configs/pool_making/base.json \
-    && python train.py --net=./lesson_6_nn_intro/configs/architectures/simple_cnn.json --learn=./lesson_6_nn_intro/configs/learning_process/adam_0001.json
