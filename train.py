ec3a9348bf2789a6fd7ec31a226324c3ed9ac547
DaveMSU <davehum@mail.ru>
2023-05-07 15:07:53 +0300

    Make code very flexiable.

--- train.py
+++ train.py
@@ -2,13 +2,15 @@ import json
 import pathlib
 import pickle
 
-import torch
 import numpy as np
+import sklearn.metrics
+import torch
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
 from lib.help_tools import make_logger, parse_args
-from lib.net_factory import NeuralNetwork
+from lib.metric_factory import MetricFactory
+from lib.net_factory import NetFactory
 
 
 def main():
@@ -60,23 +62,26 @@ def main():
         logger.debug(f"{torch.cuda.get_device_name(device_identifier)=}")
         logger.debug(f"{torch.cuda.get_device_properties(device)}")
 
-    loss_params = learning_process["hyper_params"]["loss"]
-    optimizer_params = learning_process["hyper_params"]["optimizer"]
-    epoch_nums = learning_process["hyper_params"]["epoch_nums"]
+    loss_params: dict = learning_process["hyper_params"]["loss"]
+    optimizer_params: dict = learning_process["hyper_params"]["optimizer"]
+    epoch_nums: int = learning_process["hyper_params"]["epoch_nums"]
 
-    net = NeuralNetwork(net_arch)
+    # init model from pretrained state / continue unfinished learning.
+    net = NetFactory.create_network(net_arch)
     logger.debug("Model has been created successfully with no pretrain.")
-    if learning_process["start_model"]:
+    if learning_process["continue_from"]:
         checkpoint = torch.load(
-            learning_process["start_model"],
+            learning_process["continue_from"],
             map_location=device
         )
         net.load_state_dict(checkpoint['model_state_dict'])
-        logger.debug(
-            "Pretrain init state "
-            f"`{learning_process['start_model']}` have been loaded."
-        )
+        # TODO: add here other *_state_dict loads.
         epoch_start = checkpoint["epoch"] + 1
+        logger.debug(
+            "Checkpoint"
+            f" `{learning_process['start_from']['checkpoint_path']}`"
+            " have been successfully loaded!"
+        )
     else:
         epoch_start = 0
     epoch_end = epoch_start + epoch_nums
@@ -97,8 +102,28 @@ def main():
 
     writer = SummaryWriter(learning_process["tensorboard_logs"])
     logger.debug("SummaryWriter has been created successfully.")
-    best_val_loss, best_epoch = float("inf"), 0
+
+    assert learning_process["main_metric_name"] in {
+        metric["name"] for metric in learning_process["metrics"]
+    }, f"Main metric {learning_process['main_metric_name']}"
+    " wasn't finded among metrics!"
+
+    metrics: tp.Dict[str, tp.Any] = {
+        metric["name"]: MetricFactory.create_metric(
+            metric["function"],
+            metric["params"]
+        )
+        for metric in learning_process["metrics"]
+    }
+    logger.debug(
+        "MetricFactory has created all metric handlers successfully!"
+    )
+
+    main_metric_name = learning_process["main_metric_name"]
+    best_main_metric_value = metrics[main_metric_name].worst_value
+    best_epoch = 0
     train_loss_in_the_best_epoch = float("inf")
+    val_loss_in_the_best_epoch = float("inf")
 
     # training itself.
     for epoch in range(epoch_start, epoch_end, 1):
@@ -107,7 +132,7 @@ def main():
         logger.debug("Model set to train mode.")
         loss_history = []
         for X, y, *_ in dataloaders["train"]:
-            logger.debug("All data have been erased from dataloaders['train'].")
+            logger.debug("Data have been erased from dataloaders['train'].")
             X, y = X.to(device), y.to(device)
             logger.debug("X, y tensors have been transported to device.")
             optimizer.zero_grad()
@@ -129,10 +154,12 @@ def main():
 
         net.eval()
         logger.debug("Model has been set to eval mode.")
-        loss_history = []
+        loss_history: tp.List[float] = []
+        y_true_history: tp.List[tp.List[float]] = []
+        y_pred_history: tp.List[tp.List[float]] = []
         with torch.no_grad():
             for X, y, *_ in dataloaders["val"]:
-                logger.debug("All data have been  erased from dataloaders['val'].")
+                logger.debug("Data have been erased from dataloaders['val'].")
                 X, y = X.to(device), y.to(device)
                 logger.debug("X, y tensors have been transported to device.")
                 y_pred = net(X)
@@ -140,40 +167,73 @@ def main():
                 loss_value = loss(y_pred, y)
                 logger.debug("Loss value has been calculated.")
                 loss_history.append(loss_value.cpu().data.item())
-
+                y_true_history.extend(y.cpu().data.tolist())
+                y_pred_history.extend(y_pred.cpu().data.tolist())
         val_loss = np.mean(loss_history)
         logger.info("val loss: %f" % val_loss)
-        logger.info("val quality: %f" % (val_loss * 100**2))
+
+        y_true_history: np.ndarray = np.array(y_true_history)
+        y_pred_history: np.ndarray = np.array(y_pred_history)
+
+        calculated_metrics: tp.Dict[str, float] = dict()
+        for name, metric_handler in metrics.items():
+            calculated_metrics[name] = metric_handler(
+                y_true_history,
+                y_pred_history
+            )
+            logger.info(
+                f"`{name}` value at val:  %f" % calculated_metrics[name]
+            )
+        logger.debug("All metrics have been calculated!")
 
         writer.add_scalars(
-            'Loss',
+            "Loss",
             {
-                'train': train_loss,
-                'val': val_loss
+                "train": train_loss,
+                "val": val_loss
             },
             epoch
         )
+        writer.add_scalars("Metrics", calculated_metrics, epoch)
         logger.debug("Writer have added new scalars.")
 
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
+        if metrics[main_metric_name].is_it_better_than(best_main_metric_value):
+            best_main_metric_value = calculated_metrics[main_metric_name]
             train_loss_in_the_best_epoch = train_loss
+            val_loss_in_the_best_epoch = val_loss
             best_epoch = epoch
-            torch.save(  # TODO: add loss state and chckpt with last state.
+            torch.save(
                 {
-                    'model_state_dict': net.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
-                    'epoch': epoch
+                    "model_state_dict": net.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "metric_value": best_main_metric_value,
+                    "epoch": epoch
                 },
-                pathlib.Path(learning_process["model_dump"])
+                pathlib.Path(learning_process["best_model_dump"])
             )
-            logger.info("New best dump has been achived!")
+            logger.info("New best dump has been achived and saved!")
+
+        torch.save(
+            {
+                'model_state_dict': net.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss_state_dict': loss.state_dict(),
+                'epoch': epoch,
+                'current_val_loss_value': val_loss,
+                'best_epoch': best_epoch,
+                'best_metric_value': best_main_metric_value
+            },
+            pathlib.Path(learning_process["last_model_dump"])
+        )
+        logger.debug("New last checkpoint has been saved!")
 
     logger.debug("Training completed.")
     logger.info(
-        f"Best val loss ({best_val_loss}) was achived at the end of"
-        f" {best_epoch}'th epoch (by counting from zero) (the"
-        f" corresponding train loss is {train_loss_in_the_best_epoch})."
+        f"Best `{main_metric_name}` value ({best_main_metric_value})"
+        f" was achived at the end of {best_epoch}'th epoch (by"
+        " counting from zero) (the corresponding"
+        f" train loss is {train_loss_in_the_best_epoch},"
+        f" val loss is {val_loss_in_the_best_epoch})."
     )
     writer.close()
     logger.debug("Writer closed.")
